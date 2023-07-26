a3bf14ad90f905b9c7d157b7625060836e2ee095
erdosgyorgy <mortail80@gmail.com>
2023-07-26 12:33:05 +0200

    start commit

---
+++ airport/src/test/java/hu/cobix/airport/AirportApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.cobix.airport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
